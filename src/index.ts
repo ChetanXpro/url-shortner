aaed6a1e6b939b77c5c3e834e63dce9f22de432b
Chetan Baliyan <chetanbaliyan10.com>
2023-02-02 20:42:01 +0530

    feat: basic setup

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server running`)
+})
+
